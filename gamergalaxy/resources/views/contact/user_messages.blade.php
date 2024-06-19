41af7ce1f138385b44aef30b78dc85fa918cb42e
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-19 14:35:18 +0200

    style geedit

--- gamergalaxy/resources/views/contact/user_messages.blade.php
+++ gamergalaxy/resources/views/contact/user_messages.blade.php
@@ -20,4 +20,60 @@
         </tbody>
     </table>
 </div>
+<style>
+
+    body{
+        background-color: black;
+        color: white;
+        margin: 0;
+        padding: 0;
+    }
+    .star {
+        position: absolute;
+        width: 2px;
+        height: 2px;
+        background: white;
+        animation: twinkle 1s infinite alternate;
+    }
+
+    @keyframes twinkle {
+        from {
+            opacity: 1;
+        }
+        to {
+            opacity: 0.5;
+        }
+    }
+
+    .stars-container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none; /* Zorgt ervoor dat de sterren niet de interactie met de rest van de pagina verstoren */
+        overflow: hidden;
+    }
+</style>
+<script>
+    function generateStars() {
+            const numStars = 1000;
+            const container = document.querySelector('.container');
+
+            function createStar() {
+                var star = document.createElement('div');
+                star.classList.add('star');
+                star.style.left = Math.random() * 100 + 'vw';
+                star.style.top = Math.random() * 100 + 'vh';
+                star.style.animationDuration = Math.random() * 2 + 1 + 's';
+                return star;
+            }
+
+            for (let i = 0; i < numStars; i++) {
+                var star = createStar();
+                container.appendChild(star);
+            }
+        }
+        window.addEventListener('load', generateStars);
+</script>
 @endsection
