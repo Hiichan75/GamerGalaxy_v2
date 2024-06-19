cb138a50e92cd800a5601f532a1c16188ca97820
Matthias-254 <lannoo.matthias@hotmail.com>
2024-06-19 11:30:26 +0200

    test

--- gamer galaxy/resources/views/faq/create.blade.php
+++ gamer galaxy/resources/views/faq/create.blade.php
@@ -25,4 +25,24 @@
         <button type="submit" class="btn btn-primary">Add FAQ</button>
     </form>
 </div>
+<script>
+    function generateStars() {
+    const numStars = 1000;
+    const container = document.querySelector('.gamelist');
+
+    function createStar() {
+        var star = document.createElement('div');
+        star.classList.add('star');
+        star.style.left = Math.random() * 100 + 'vw';
+        star.style.top = Math.random() * 100 + 'vh';
+        star.style.animationDuration = Math.random() * 2 + 1 + 's';
+        return star;
+    }
+
+    for (let i = 0; i < numStars; i++) {
+        var star = createStar();
+        container.appendChild(star);
+    }
+}
+</script>
 @endsection
